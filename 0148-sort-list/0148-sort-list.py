965d8c4a4a30bb2a29df31d17aa356c3764fa685
xxpopielxx <143009372+xxpopielxx@users.noreply.github.com>
2025-03-28 19:56:08 +0100

    Time: 199 ms (34.39%), Space: 32.9 MB (41.93%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -3,13 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# Definition for singly-linked list.
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
         mid = self.find_middle(head)
+
         left = head
         right = mid.next
         mid.next = None  
@@ -19,29 +18,28 @@ class Solution:
 
         return self.merge(left_sorted, right_sorted)
 
-    def find_middle(self, head: ListNode) -> ListNode:
-        slow, fast = head, head
+    def find_middle(self, head):
+        slow = fast = head
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         return slow
 
-    def merge(self, a: ListNode, b: ListNode) -> ListNode:
-        dummy = ListNode()
-        tail = dummy
-
-        while a and b:
-            if a.val < b.val:
-                tail.next = a
-                a = a.next
+    def merge(self, left, right):
+        dummy = ListNode(None)
+        current = dummy
+        while left and right:
+            if left.val < right.val:
+                current.next = left
+                left = left.next
             else:
-                tail.next = b
-                b = b.next
-            tail = tail.next
-
-        if a:
-            tail.next = a
-        if b:
-            tail.next = b
-
+                current.next = right
+                right = right.next
+            current = current.next
+        if left:
+            current.next = left
+        else:
+            current.next = right
         return dummy.next
+        
+
