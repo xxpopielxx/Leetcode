bf6efcf94a6657458433a25f88682bc45a06feaa
xxpopielxx <143009372+xxpopielxx@users.noreply.github.com>
2025-07-14 19:13:55 +0200

    Time: 15 ms (95.58%), Space: 18 MB (54.42%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.py
@@ -1,29 +1,41 @@
+import bisect
+
 class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         n = len(nums)
         if n < 3:
             return 0
-
-        #LIS
-        lis = [1] * n
-        for i in range(1,n):
-            for j in range(i):
-                if nums[j] < nums[i] and lis[j] + 1 > lis[i]:
-                    lis[i] = lis[j] + 1
-
-        #LDS
-        lds = [1] * n
-        for i in range(n-1, -1 ,-1):
-            for j in range(i+1,n):
-                if nums[i] > nums[j] and lds[j] + 1 > lds[i]:
-                    lds[i] = lds[j] + 1
-        maxi = 0
-        for i in range(1, n-1):
-            if lis[i] > 1 and lds[i] > 1:
-                current = lis[i] + lds[i] - 1
-                maxi = max(maxi, current)
         
-        return n - maxi if maxi >= 3 else n - 3
+        # LIS nlogn
+        lis = [1] * n
+        tails = []
+        for i in range(n):
+            idx = bisect.bisect_left(tails, nums[i])
+            if idx == len(tails):
+                tails.append(nums[i])
+            else:
+                tails[idx] = nums[i]
+            lis[i] = idx + 1
+        
+        # LDS nlogn
+        lds = [1] * n
+        tails = []
+        for i in range(n-1, -1, -1):
+            idx = bisect.bisect_left(tails, nums[i])
+            if idx == len(tails):
+                tails.append(nums[i])
+            else:
+                tails[idx] = nums[i]
+            lds[i] = idx + 1
+        
+        max_mountain = 0
+        for i in range(n):
+            if lis[i] > 1 and lds[i] > 1:
+                current_mountain = lis[i] + lds[i] - 1
+                if current_mountain > max_mountain:
+                    max_mountain = current_mountain
+        
+        return n - max_mountain if max_mountain >= 3 else n - 3
 
 
         
