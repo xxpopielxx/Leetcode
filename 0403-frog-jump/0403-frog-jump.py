e9f77205c1967679d2ead18f8c08b5c3acc42a15
xxpopielxx <143009372+xxpopielxx@users.noreply.github.com>
2025-08-08 19:45:35 +0200

    Time: 319 ms (23.66%), Space: 18.4 MB (99.62%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -7,21 +7,17 @@ class Solution:
         
         for i in range(n):
             for last_jump in dp[i]:
-                for jump in (last_jump - 1, last_jump, last_jump + 1):
-                    if jump > 0:
-                        next_pos = stones[i] + jump
+                for j in range(i+1, n):
+                    jump = stones[j] - stones[i]
+                    if jump < last_jump - 1:
+                        continue
+                    if jump > last_jump + 1:
+                        break # dalsze kamienie są jeszcze dalej, więc można przerwać
+                    if jump not in dp[j]:
+                        dp[j].append(jump)
 
-                        for j in range(i+1, n):
-                            if stones[j] == next_pos:
-                                if jump not in dp[j]:
-                                    dp[j].append(jump)
-                                break
-                            elif stones[j] > next_pos:
-                                break
+        return len(dp[-1]) > 0
         
-        if len(dp[n-1]) > 0:
-            return True
-        return False
 
 
 
