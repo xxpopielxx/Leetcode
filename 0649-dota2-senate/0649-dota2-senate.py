f58c389f7dde4e79ebc5da34f0bded00ff7b634a
xxpopielxx <143009372+xxpopielxx@users.noreply.github.com>
2025-08-18 12:25:32 +0200

    Time: 10 ms (75.34%), Space: 18.4 MB (19.19%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,23 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        radiant = deque()
+        dire = deque()
+
+        for i in range(n):
+            if senate[i] == "R":
+                radiant.append(i)
+            else:
+                dire.append(i)
+        
+        while radiant and dire:
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d:
+                radiant.append(r + n)
+            else:
+                dire.append(d + n)
+        
+        return "Radiant" if radiant else "Dire"
+        
