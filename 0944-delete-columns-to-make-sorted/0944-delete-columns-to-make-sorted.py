6968db4730a34d545ac021f7b66729343ba48167
Kamil Kaczmarczyk <143009372+xxpopielxx@users.noreply.github.com>
2025-12-20 11:59:14 +0100

    Time: 63 ms (59.77%), Space: 18.1 MB (99.28%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,18 +1,15 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        n = len(strs[0])
-        if len(strs) == 1:
-            return 0
-        prev = [s for s in strs[0]]
-        deleted = [False] * n
+        rows = len(strs)
+        cols = len(strs[0])
         res = 0
-
-        for x in range(1, len(strs)):
-            for i in range(n):
-                if strs[x][i] < prev[i] and not deleted[i]:
+        
+        for c in range(cols):
+            for r in range(1, rows):
+                if strs[r][c] < strs[r-1][c]:
                     res += 1
-                    deleted[i] = True
-                prev[i] = strs[x][i]
+                    break
+                    
         return res
 
 
