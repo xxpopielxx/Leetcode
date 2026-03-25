19f7948f317c92de13ded78eed8334b5d600d10b
Kamil Kaczmarczyk <143009372+xxpopielxx@users.noreply.github.com>
2026-03-25 23:00:31 +0100

    Time: 3 ms (88.56%), Space: 25 MB (9.76%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,23 +1,22 @@
 from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        counter = Counter(nums)
-        buckets = [[] for _ in range(n+1)]
+        cnt = Counter(nums)
 
-        for num, freq in counter.items():
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, freq in cnt.items():
             buckets[freq].append(num)
         
-        result = []
-        for i in range(n, -1, -1):
-            if buckets[i]:
-                for num in buckets[i]:
-                    result.append(num)
-                    if len(result) == k:
-                        return result
-        return result
+        res = []
+        for i in range(len(buckets) -1, 0, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
 
+        
         
         
             
